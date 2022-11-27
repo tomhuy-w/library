339ab05a947f46f259c185253506a4622ec16235
wells <jhang0912407249@gmail.com>
2022-11-27 23:11:13 +0100

    feat: add scaffold book

---
+++ db/migrate/20221127220759_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.integer :year
+      t.integer :edition
+      t.string :publisher
+      t.integer :quntity
+
+      t.timestamps
+    end
+  end
+end
