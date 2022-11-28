922ea32ac15925bd54243d52b23491b7ce48cf0a
wells <jhang0912407249@gmail.com>
2022-11-28 19:56:09 +0100

    feat: user cannot borrow book without log in

--- app/controllers/borrower_records_controller.rb
+++ app/controllers/borrower_records_controller.rb
@@ -1,5 +1,7 @@
 class BorrowerRecordsController < ApplicationController
   before_action :set_book, only: [:create]
+  before_action :authenticate_user!
+
   def create
     BookService.new(@book, current_user).borrow!
     redirect_to books_path, notice: 'borrowed successfully'
