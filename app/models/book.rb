d86b9a42b70c8721dc755f413c7002700e7ac046
wells <jhang0912407249@gmail.com>
2022-11-28 20:37:14 +0100

    feat: book#quantity must greater than or equal to 0

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@
 
 class Book < ApplicationRecord
   has_many :borrower_records, dependent: :destroy
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
 
   def can_borrow?
     quantity.positive?
