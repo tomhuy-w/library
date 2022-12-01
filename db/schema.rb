969f686edaa811e461a60a4bfa4e105675ae9977
wells <jhang0912407249@gmail.com>
2022-12-01 02:06:11 +0100

    Refactor: remove no useful schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_010047) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_010428) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -33,16 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_010047) do
     t.index ["user_id"], name: "index_borrower_records_on_user_id"
   end
 
-  create_table "borrowerrecord_tables", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "book_id", null: false
-    t.datetime "returned_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_borrowerrecord_tables_on_book_id"
-    t.index ["user_id"], name: "index_borrowerrecord_tables_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -57,6 +47,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_010047) do
 
   add_foreign_key "borrower_records", "books"
   add_foreign_key "borrower_records", "users"
-  add_foreign_key "borrowerrecord_tables", "books"
-  add_foreign_key "borrowerrecord_tables", "users"
 end
