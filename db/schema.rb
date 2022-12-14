bd54a90f81c4642b72a2e6246cb216e0f7a95c1c
wells <jhang0912407249@gmail.com>
2022-12-14 22:39:26 +0100

    feat: add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_204333) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_213829) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_204333) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
